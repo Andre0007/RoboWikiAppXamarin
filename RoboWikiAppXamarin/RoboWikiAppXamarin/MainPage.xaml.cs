41658260fbd641613047703b6f420271abd6ef12
André <aa.concha7@gmail.com>
2020-05-31 13:00:14 -0300

    Primeiro Commit Projeto de pesquisa em um app que acessa a base do WikiPedia

---
+++ RoboWikiAppXamarin/RoboWikiAppXamarin/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Algorithmia;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace RoboWikiAppXamarin
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+           InitializeComponent();
+        }
+
+        private void btExecutar_Clicked(object sender, EventArgs e)
+        {
+            try { 
+                var input = "{"
+                            + "  \"articleName\": \"" + eValor.Text + "\","
+                            + "  \"lang\": \"pt\""
+                            + "}";
+
+                var client = new Client("sim2JF6Ncdn7FkDG+7QomvK2j+V1");
+                var algorithm = client.algo("web/WikipediaParser/0.1.2");
+                algorithm.setOptions(timeout: 300); // optional
+                var response = algorithm.pipeJson<DadosWikipedia>(input);
+
+                DadosWikipedia dados = (DadosWikipedia)response.result;
+                this.Navigation.PushAsync(new PagePesquisa(dados));
+            }
+            catch (Exception ex) {
+                DisplayAlert("Error", "Tente refinar os parâmetros de busca, erro: " + ex.Message, "OK");
+            }
+        }
+    }
+}
---
+++ RoboWikiAppXamarin/RoboWikiAppXamarin/PagePesquisa.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RoboWikiAppXamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PagePesquisa : ContentPage
+    {
+        public PagePesquisa()
+        {
+            InitializeComponent();
+        }
+
+        public PagePesquisa(DadosWikipedia dados)
+        {
+            InitializeComponent();
+            lTitulo.Text = dados.title;
+            GetImages(dados);
+            //iImagem.Source = dados.images[0];
+            eDados.Text = dados.content.Substring(0, dados.content.IndexOf("="));
+        }
+
+        public String GetImage(DadosWikipedia dados)
+        {
+            string imagem = "";
+            int i = 0;
+            while (i < dados.images.Length && imagem == "")
+            {
+                if (dados.images[i].IndexOf("jpg") > 0)
+                {
+                    imagem = dados.images[i];
+                }
+                i++;
+            }
+            return imagem;
+        }
+
+        public void GetImages(DadosWikipedia dados)
+        {
+            string imagem = "";
+            int i = 0;
+            while (i < dados.images.Length)
+            {
+                if (dados.images[i].IndexOf("jpg") > 0)
+                {
+                    imagem = dados.images[i];
+                    Image image = new Image { Source = imagem, HeightRequest = 300 };
+                    sImagens.Children.Add(image);
+                }
+                i++;
+            }
+        }
+
+    }
+}
